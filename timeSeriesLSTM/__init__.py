7ddaa1a9c676a91637a11bfc4d57ad400f358ffa
Benson <bensonchen@cht.com.tw>
2018-08-07 17:40:18 +0800

    example stateful LSTM

--- timeSeriesLSTM/__init__.py
+++ timeSeriesLSTM/__init__.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import time
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+#
+# now = time.ctime(int(time.time()))
+# print(str(now))
 
-now = time.ctime(int(time.time()))
-print(str(now))
+for i in range(1, 5):
+    value = i
+    print(value)
